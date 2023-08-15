ee3c041f73e28bde9f23179693a26e4bcb89a2e7
OmarLocked10 <omaribrahimsalah956@gmail.com>
2023-08-15 18:52:33 +0300

    adding my projects

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -17,16 +17,16 @@ const Projects = () => {
           Recent Top <strong className="yellow">Works </strong>
         </h1>
         <p style={{ color: "white" }}>
-          Here are a few projects I've worked on recently.
+          Here are a few projects I've worked on
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={lift}
               isBlog={false}
-              title="Lift-Buddy"
-              description="This online ride-sharing platform boasts a responsive front-end design, complete with an admin panel built using React and Material-UI. With the integration of Google Maps API, users can easily access directions on the map. The platform allows for full CURD functionality on profiles and posts, and users can confirm or cancel a ride from a post and message each other after confirmation. Additionally, users can easily reset their password. The back-end is powered by a Node Express server and utilizes MongoDB for the database management."
-              ghLink="https://github.com/19sajib/lift-buddy"
+              title="Audio Emotion Recognition"
+              description="A Deep Learning Algorithm combined with some vocal analysis to identify emotions."
+              ghLink="https://colab.research.google.com/drive/1o66eIDcF4jWDrRXNbC7dl4_UnK1WdpVv?usp=sharing"
             />
           </Col>
 
@@ -34,9 +34,9 @@ const Projects = () => {
             <ProjectCard
               imgPath={project}
               isBlog={false}
-              title="Social Media"
-              description="This platform features a responsive interface built with Reactjs and CSS, allowing users to easily manage their profiles and posts through CRUD activities. The option to follow other users and interact with their posts (like or dislike) is also available, with suggestions for new users to follow. Users can only view posts from their followed users and can only engage in chat with those they follow. The backend is powered by Nodejs and Expressjs server, while MongoDB is utilized as the database."
-              ghLink="https://github.com/19sajib/social-media"
+              title="Twitter Sentiment Analysis"
+              description="A deep learning algorithm to empower financial decisions based on sentiment analysis"
+              ghLink="https://colab.research.google.com/drive/1fOI8I1ovXcdCXexMK5tc5CdJ2WQF45NT?usp=sharing"
             />
           </Col>
 
